eed5d0a69e24f07eb5499d9f85a488265420949d
Thorbjørn Steen <therubberduckmail@gmail.com>
2018-07-25 16:55:54 +0200

    Added shortcut buttons for group numbers

--- app/src/main/java/com/dicemonger/campaignmanager/Frontend/Screens/Initiative/GroupPickerDialog.kt
+++ app/src/main/java/com/dicemonger/campaignmanager/Frontend/Screens/Initiative/GroupPickerDialog.kt
@@ -5,6 +5,7 @@ import android.app.AlertDialog
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.GridLayout
 import com.dicemonger.campaignmanager.R
 
 interface GroupPickerListener{
@@ -18,18 +19,38 @@ class GroupPickerDialog(_context: Activity, val _item: CombatantDbo, val _listen
     val _edtNumber : EditText
     val _btnAdd : Button
 
+    val _grdNumbers : GridLayout
+
     init {
         _view = _context.layoutInflater.inflate(R.layout.dialog_grouppicker, null)
         setView(_view)
 
+        setTitle(_item.name + "s")
         setCancelable(true)
 
         _edtNumber = _view.findViewById(R.id.edtNumber)
         _btnAdd = _view.findViewById(R.id.btnAdd)
 
+        _grdNumbers = _view.findViewById(R.id.grdNumbers)
+
         _btnAdd.setOnClickListener {addGroup() }
 
+        var i = 0
+        repeat(_grdNumbers.childCount) {
+            val btn = _grdNumbers.getChildAt(i) as Button
+            btn.setOnClickListener { clickedNumberButton(btn) }
+            i++
+        }
+
         show()
+    }
+
+    private fun clickedNumberButton(button: Button) {
+        val text = button.text
+        _item.groupNumber = text.toString().toInt()
+        _listener.groupAdded(_item)
+        cancel()
+
     }
 
     fun addGroup() {
